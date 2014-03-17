18af3aa64a887fd20f165f94eeda16855abceba4
Jeff Rhode <jeffrhode@Jeffs-MacBook-Pro.local>
2014-03-16 20:45:16 -0700

    added test class

---
+++ JRTest.h
@@ -0,0 +1,13 @@
+//
+//  JRTest.h
+//  get test
+//
+//  Created by Jeff Rhode on 3/16/14.
+//  Copyright (c) 2014 Jeff Rhode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JRTest : NSObject
+
+@end
---
+++ JRTest.m
@@ -0,0 +1,13 @@
+//
+//  JRTest.m
+//  get test
+//
+//  Created by Jeff Rhode on 3/16/14.
+//  Copyright (c) 2014 Jeff Rhode. All rights reserved.
+//
+
+#import "JRTest.h"
+
+@implementation JRTest
+
+@end
